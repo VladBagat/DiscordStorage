e57b94143606a3f09662b36c772a0c0c14a6138e
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2025-03-04 10:15:25 +0000

    Added !info commmand and !upload messages

--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -17,6 +17,7 @@ use std::path::Path;
 use std::fs::create_dir_all;
 use serenity::all::Ready;
 use serenity::all::EditMessage;
+use uuid::Uuid;
 
 struct Handler {}
 
@@ -28,58 +29,93 @@ impl EventHandler for Handler {
 
     async fn message(&self, ctx: Context, msg: Message) {
 
-        let command;
-        let target;
-
-        if msg.author.bot{
+        if msg.author.bot {
             return;
         }
-        match strip_args(&msg.content){
-            Some((com, tar)) => {
-                command = com.to_lowercase();
-                target = tar.to_string();
-            }
+
+        let (command, target, name) = match strip_args(&msg.content) {
+            Some((com, tar, tar_name)) => (
+                com.to_lowercase(),
+                tar.to_string(),
+                tar_name.to_string(),
+            ),
             None => {
-                msg.channel_id.say(&ctx.http, "Invalid command. Use !info for more information").await.unwrap();
+                if let Err(e) = msg.channel_id.say(&ctx.http, "Invalid command. Use !info for more information").await {
+                    eprintln!("Failed to send message: {:?}", e);
+                }
                 return;
+            }
+        };
+
+        match command.as_str() {
+            "!upload" => {
+                let id: String = Uuid::new_v4().to_string();
+                match msg.channel_id.say(&ctx.http, format!("Upload: {}. ID: {}", name, &id)).await {
+                    Ok(_) => {
+                        msg.channel_id.say(&ctx.http, "====START OF UPLOAD====")
+                            .await.expect("Failed to send upload start status message");
+                        let file_paths: Vec<std::path::PathBuf> = deconstruct(&target).unwrap();
+                        for path in file_paths {
+                            send_file(&ctx, &msg, path).await;
+                        }
+                        msg.channel_id.say(&ctx.http, "====END OF UPLOAD====")
+                            .await.expect("Failed to send upload success status message");
+                    },
+                    Err(why) => println!("Error sending message: {why:?}"),
+                }        
+            },
+            "!download" => {
+                let mut messages = msg.channel_id.messages_iter(&ctx).boxed();
+                while let Some(message_result) = messages.next().await {
+                    match message_result {
+                        Ok(message) => {
+                            if message.author.bot && message.attachments.len() > 0 {
+                                let download_path = message.attachments[0].url.clone();
+                                download_file(&download_path, &message.content).await;
+                                }
+                        },
+                        Err(error) => eprintln!("Uh oh! Error: {}", error),
+                    }
+                }
+                match msg.channel_id.say(&ctx.http, "Trying to call algorithm").await {
+                    Ok(_) => {
+                        reconstruct().unwrap();
+                        msg.channel_id.say(&ctx.http, "Installation finished successfully")
+                            .await.expect("Failed to send upload success status message");
+                    },
+                    Err(why) => println!("Error sending message: {why:?}"),
+                }
+            },
+            "!info" => {
+                let message: &str = "Welcome to DiscordStore! You are currently running version 1.\n\
+                     \n\
+                     You have access to the following commands:\n\
+                     \n\
+                     - `!upload <path> <name>`\n\
+                       This command will search the given path (preferably absolute) and upload files to the current channel.
+                       You **must** upload no more than 1 project per channel.\n\
+                     \n\
+                     - `!download`\n\
+                       This command will download and format uploaded files in the current channel.\n\
+                       Find the downloaded project in the same directory as the `discorstore.exe` file.";
+
+                msg.channel_id.say(&ctx.http, message)
+                        .await.expect("Failed to send !info command message");
+            },
+            _ => {
+                msg.channel_id.say(&ctx.http, "Unknown command. Use !info for help.")
+                        .await.expect("Failed to send unknown command message");
             }
         }
         
         if command == "!upload" {
-            match msg.channel_id.say(&ctx.http, "Trying to call algorithm").await {
-                Ok(mut bot_message) => {
-                    let mut files_sent = 0;
-                    let file_paths: Vec<std::path::PathBuf> = deconstruct(&target).unwrap();
-                    let file_amount = file_paths.len();
-                    bot_message.edit(&ctx.http, EditMessage::new().content(
-                        format!("Found {} files", file_paths.len()))).await.unwrap();
-                    for path in file_paths {
-                        send_file(&ctx, &msg, path).await;
-                        files_sent += 1;
-                        bot_message.edit(&ctx.http, EditMessage::new().content(
-                            format!("Uploaded {}/{} files", files_sent, &file_amount))).await.unwrap();
-                    }
-                },
-                Err(why) => println!("Error sending message: {why:?}"),
-            }
+                                
         }
         else if command == "!download" {
-            let mut messages = msg.channel_id.messages_iter(&ctx).boxed();
-            while let Some(message_result) = messages.next().await {
-                match message_result {
-                    Ok(message) => {
-                        if message.author.bot && message.attachments.len() > 0 {
-                            let download_path = message.attachments[0].url.clone();
-                            download_file(&download_path, &message.content).await;
-                            }
-                    },
-                    Err(error) => eprintln!("Uh oh! Error: {}", error),
-                }
-            }
-            match msg.channel_id.say(&ctx.http, "Trying to call algorithm").await {
-                Ok(_) => {reconstruct().unwrap();},
-                Err(why) => println!("Error sending message: {why:?}"),
-            }
+            
+        }
+        else {
+            
         }
     }
 }
@@ -103,26 +139,18 @@ async fn download_file(download_path: &str, save_path: &str) {
     io::copy(&mut resp.bytes().await.unwrap().as_ref(), &mut out).expect("failed to copy content");
 }           
 
-fn strip_args(user_message: &str) -> Option<(&str, &str)> {
-    let mut args: Vec<&str> = user_message.split_whitespace().collect();
-    if args.len() == 1 {
-       args.push("Empty");
-    }
-    let command = args[0];
-    let target = args[1];
-
-    Some((command, target))
+fn strip_args(user_message: &str) -> Option<(&str, &str, &str)> {
+    let args: Vec<&str> = user_message.split_whitespace().collect();
+    let command = args.get(0).unwrap_or(&"Empty");
+    let target = args.get(1).unwrap_or(&"Empty");
+    let name = args.get(2).unwrap_or(&"Empty");
+    Some((command.to_owned(), target.to_owned(), name.to_owned()))
 }
 
 #[tokio::main]
 pub async fn discord(token: &str) -> Result<(), serenity::Error> {
 
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
-
-    // Create a new instance of the Client, logging in as a bot.
-    let mut client = Client::builder(token, intents)
+    let mut client = Client::builder(token,  GatewayIntents::all())
     .event_handler(Handler{}).await
     .map_err(|why| {return why})?;
 
